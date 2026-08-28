65d9d58068dc17755019477f1a5d67581f855f85
Pintu <deadpoolpintu3690@gmail.com>
2026-08-28 15:38:21 +0530

    Mark api/case-study.js as deprecated - DELETE THIS FILE

--- api/case-study.js
+++ api/case-study.js
@@ -1,32 +1,3 @@
-import * as data from '../data.js';
-
-export default function handler(request, response) {
-  if (request.method !== "GET") {
-    response.setHeader("Allow", "GET");
-    return response.status(405).json({ error: "Method not allowed" });
-  }
-
-  return response.status(200).json({
-    meta: data.meta,
-    liveTelemetryInitial: data.liveTelemetryInitial,
-    kpis: data.kpis,
-    founder: data.founder,
-    unlistedShareMarket: data.unlistedShareMarket,
-    howCredWorks: data.howCredWorks,
-    gatingMechanics: data.gatingMechanics,
-    businessModel: data.businessModel,
-    productEcosystem: data.productEcosystem,
-    valuationTimeline: data.valuationTimeline,
-    totalCapitalRaised: data.totalCapitalRaised,
-    financials: data.financials,
-    arpuBenchmark: data.arpuBenchmark,
-    unitEconomics: data.unitEconomics,
-    swot: data.swot,
-    pestle: data.pestle,
-    competitors: data.competitors,
-    strategicOutlook: data.strategicOutlook,
-    keyLessons: data.keyLessons,
-    intelFAQ: data.intelFAQ,
-    sources: data.sources
-  });
-}
+// DEPRECATED: This file should be deleted
+// Use api/case-study.mjs instead for Vercel deployment
+// To remove this file, run: git rm api/case-study.js && git commit -m "Remove duplicate CommonJS handler"
